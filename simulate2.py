306824ef3b71d562d6b9864134c3dcbc374c4a84
thapakorn pheyporn <thapakorn613@gmail.com>
2018-11-22 02:53:38 +0700

    update return and ()

--- simulate2.py
+++ simulate2.py
@@ -9,14 +9,14 @@ def BEQ_I_TYPE(machine,PCindex):
     regB = int(machine[13:16],2) #select_bit 18-16
     Offsetfield = machine[16:] #select_bit 15-0
     
-    print "\t\tBEQ"
+    print ("\t\tBEQ")
     
     if(registers[regA] == registers[regB]):
         PCindex=PCindex+CovertTwocomplement(Offsetfield)
-        print "JUME !!"
+        print ("JUME !!")
         return PCindex
     else:
-        print "NOT JUME !!"
+        print ("NOT JUME !!")
         return PCindex
 
 def JAIR_J_TYPE(machine,PCindex):
@@ -24,7 +24,7 @@ def JAIR_J_TYPE(machine,PCindex):
     regA = int(machine[10:13],2) #select_bit 21-19 #keep address
     regB = int(machine[13:16],2) #select_bit 18-16 #keep PC+1
     
-    print "\t\tJAIR"
+    print ("\t\tJAIR")
     
     if (registers[regA]==registers[regB]):
         
@@ -39,12 +39,10 @@ def JAIR_J_TYPE(machine,PCindex):
 
     
 def HAIT_O_TYPE():
-    
-    print "\t\tHALT"
+    return ;
     
 def NOOP_O_TYPE():
-    
-    print "\t\tNOOP"
+    return ;
     
 def CovertTwocomplement(_Codebit):
     
@@ -84,67 +82,41 @@ def simulate(mem_machine_code):
     pc_idex=0
     startProgram = True
     while startProgram:
-        
         machineCode = mem_machine_code[pc_count]
         opCode = machineCode[7:10]
-        
         print ("status :")
-        print "\tPC [%(PCcount)d]"  % {"PCcount": pc_count}
-
-        #print ("\t\ttotal :"+opCode+regA+regB+Other)
-        #print ("\t\tOpcode :"+opCode)
-        #print ("\t\tregA :"+regA)
-        #print ("\t\tregB :"+regB)
-        #print ("\t\tOther :"+Other)
-        print "\tMEM :"
+        print ("\tPC [%(PCcount)d]"  % {"PCcount": pc_count})
+        print ("\tMEM :")
         for x in  range(0,MaxPc):
             #print mem_machine_code[x]
-            print "\t\tmem[%(pc)d]: %(memory)d -->%(machinebit)d Bit" \
+            print ("\t\tmem[%(pc)d]: %(memory)d -->%(machinebit)d Bit" \
                   %{"pc": x ,"memory" : binToDecimal(mem_machine_code[x])
-                      ,"machinebit":len(mem_machine_code[x])}
-           
-        print "\tREGISTERS :"
+                      ,"machinebit":len(mem_machine_code[x])})
+        print ("\tREGISTERS :")
         for y in range(len(registers)):
-            print "\t\treg[%(register)d]: %(valueInreg)d" %{"register" : y,"valueInreg" : registers[y]}
-    
+            print ("\t\treg[%(register)d]: %(valueInreg)d" %{"register" : y,"valueInreg" : registers[y]})
         if(opCode=='100'):
-            
             pc_count=BEQ_I_TYPE(machineCode,pc_count)
-            
         elif(opCode == '101'):
-            
             pc_count=JAIR_J_TYPE (machineCode,pc_count)-1
-        
         elif(opCode == '110'):
-            
             HAIT_O_TYPE()
-            
         elif(opCode == '111'):
-            
             NOOP_O_TYPE()
-            
         elif(opCode == '000'):
-            #Test opcode add
             registers[1] = registers[1]-1
-            
         else:
             print ("\t\tInvalid Opcode")
-        
-        
-        print "end status"
-        
+        print ("end status")
         if(mem_machine_code[pc_count+1][7:10]=='110'):
-            print "machine halted"
-            print "total of %(totalpc)d instructions executed" %{"totalpc":pc_idex+1}
-            print "final state of machine :"
+            print ("machine halted")
+            print ("total of %(totalpc)d instructions executed" %{"totalpc":pc_idex+1})
+            print ("final state of machine :")
         elif(mem_machine_code[pc_count][7:10]=='110'):
             startProgram = False
-        print "++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++"
+        print ("++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++")
         pc_count=pc_count+1
         pc_idex=pc_idex+1
     return;
-
-
 simulate(mem_machine_code)
-#print CovertTwocomplement('1111111111111111')
 
